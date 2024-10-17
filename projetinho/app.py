3fcaade75b3079a80954b1e4aa1b3c4eed1f1df9
MayMatilde <135733303+MayMatilde@users.noreply.github.com>
2024-10-17 19:28:02 -0300

    Update app.py

--- projetinho/app.py
+++ projetinho/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, session, redirect, url_for, request, render_template
 from controllers.controller import vamos_arrasar
 
 app = Flask(__name__)
 app.register_blueprint(vamos_arrasar)
+app.secret_key = 'JesusVive'
+
 
 if __name__ == '__main__':
     app.run(debug=True)

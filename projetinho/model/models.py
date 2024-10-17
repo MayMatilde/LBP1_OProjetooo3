905b0ffcc4a9149024001a3eb4a1986d2b0543f3
MayMatilde <135733303+MayMatilde@users.noreply.github.com>
2024-10-17 19:26:55 -0300

    Update models.py

--- projetinho/model/models.py
+++ projetinho/model/models.py
@@ -1,13 +1,7 @@
-class Livro:
-    def __init__(self, id, nome, autor, gosto, ):
-        self.nome = nome
-        self.autor = autor
-        self.gosto = gosto 
-        self.id = id
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password 
 
-listaLivros = []
-
-def AddLivros( nome, autor, gosto,):
-    id = len(listaLivros) + 1
-    novo_livro = Livro (id, nome, autor, gosto,)
-    listaLivros.append(novo_livro)
+def validate(self, username, password):
+    return self.username == username and self.password == password
